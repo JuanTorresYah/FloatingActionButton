f1a46f952b11e4db5940827141ba7c9e62ed3a32
JuanTorresYah <juan.torres@cbtis72.edu.mx>
2019-05-14 20:01:03 -0500

    Se agregó una animación al floating action button

--- app/src/main/java/com/example/floatingactionbutton/MainActivity.java
+++ app/src/main/java/com/example/floatingactionbutton/MainActivity.java
@@ -1,24 +1,72 @@
 package com.example.floatingactionbutton;
 
+//Se agregó el paquete android.animation.Animator, .AnimationUtils y Interpolator
+import android.animation.Animator;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Crea
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*Se comienza a animar el floating action button y el cual comenzará en una escala 0 y terminará en su tamaño original, el codigo solo sirve en versiones de Lollipop
+        o superiores*/
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setScaleX(0);
+        fab.setScaleY(0);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            final Interpolator interpolador = AnimationUtils.loadInterpolator(getBaseContext(), android.R.interpolator.fast_out_slow_in);
+
+            fab.animate()
+                    .scaleX(1)
+                    .scaleY(1)
+                    .setInterpolator(interpolador)
+                    .setDuration(600)
+                    .setStartDelay(1000)
+                    .setListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                           fab.animate()
+                                   .scaleY(0)
+                                   .scaleX(0)
+                                   .setInterpolator(interpolador)
+                                   .setDuration(600)
+                                   .start();
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
+        }
+
+        //Se le asigna una accion al FAB que se activa cuando es presionado
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
